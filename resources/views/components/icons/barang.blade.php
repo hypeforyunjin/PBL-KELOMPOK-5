e0350d6e2a127a86b6469521b6a2eb02464cd50b
arlafelda <arlafelda321@gmail.com>
2025-04-28 13:14:33 +0700

    arla tampilan produk

---
+++ resources/views/components/icons/barang.blade.php
@@ -0,0 +1,8 @@
+<svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <!-- Bagian atas (tutup kotak) -->
+    <rect x="5" y="4" width="14" height="4" rx="1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <!-- Bagian bawah (badan kotak) -->
+    <rect x="5" y="8" width="14" height="12" rx="2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <!-- Garis melengkung di tengah -->
+    <path d="M9 12h6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+</svg>
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,33 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\admin\AuthAdminController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
+
+// Halaman awal: login customer
 Route::get('/', function () {
     return view('customer.auth.login');
 });
 
-
+// Dashboard untuk user login
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+// Route untuk user terautentikasi
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Route admin
 Route::get('admin/login', [AuthAdminController::class,'Index'])->name('admin.login');
 Route::post('admin/login', [AuthAdminController::class,'AuthAdmin'])->name('admin.loginSubmit');
 Route::get('/admin/dashboard', [AuthAdminController::class,"Dashboard"])->name('admin.dashboard')->middleware('admin');
+
+Route::get('/produk-gorden', [ProductController::class, 'index']);
 
 require __DIR__.'/auth.php';
